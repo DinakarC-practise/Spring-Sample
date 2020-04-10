4ef6d5af32dd9db7bf7bc00026d09494eee92216
Dinakar Chandolu <chandolugus16056@gmail.com>
2020-04-10 17:43:30 -0400

    mask2

---
+++ mask 2/src/main/java/com/dinakar/mask/MaskMobileNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MaskMobileNumber {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        int len = str.length();
+        StringBuilder sb = new StringBuilder(len);
+        for (len = 6; len < 9; len++) {
+            sb.append("*");
+        }
+        String outM = str.substring(0, 5) + sb + str.substring(9, 14);
+        System.out.println(outM);
+    }
+}
